bcb2249d29b4d57d6762e4c20bff52cc112a3868
meck <meckgalen@gmail.com>
2023-07-12 13:06:01 +0300

    Fully functional script integrated

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
   const game = (() => {
     //Select game board
     const board = document.querySelector('.board');
+
+    //Select winner text
+    const endGameMessage = document.querySelector('.endGame');
     
     //Define game board array and assign them in DOM
     const boardArray = ['a1', 'a2', 'a3', 'b1', 'b2', 'b3', 'c1', 'c2', 'c3'];
@@ -49,48 +52,48 @@
     };
 
     const reset = () => {
-      for(let i = 0; i< 9; i++){
+      for(let i = 0; i < 9; i++){
         definedElements[i].textContent = '';
       };
-      playerArray = ['', '', '', '', '', '', '', '', ''];
+      for(let i = 0; i < 9; i++) {
+        playerArray[i] = '';
+      }
       currentPlayerIndex = 0;
+      endGameMessage.textContent = '';
     };
     
-    return { board, definedElements, boardArray, playerArray, getCurrentPlayer, switchTurn, checkWinner, checkDraw, reset};
+    return { board, endGameMessage, definedElements, boardArray, playerArray, getCurrentPlayer, switchTurn, checkWinner, checkDraw, reset};
   })();
 
   const player = (name, mark) => {
     return {name, mark}
   }
 
-  const player1 = player('player1', 'X');
-  const player2 = player('player2', 'O');
+  const player1 = player('Player1', 'X');
+  const player2 = player('Player2', 'O');
 
   game.board.addEventListener('click', (e) => {
     // Check if the clicked tile is valid and not already marked
     const index = game.boardArray.indexOf(e.target.id);
-    if (index !== -1 && game.playerArray[index] === '') {
-      // Get the current player object based on the turn
-      const currentPlayer = game.getCurrentPlayer();
-
+    // Get the current player object based on the turn
+    const currentPlayer = game.getCurrentPlayer();
+    if (index !== -1 && game.playerArray[index] === '' && game.checkWinner(currentPlayer.mark) !== true) {
        // Update the DOM and game state with the player's mark
       game.definedElements[index].textContent = currentPlayer.mark;
       game.playerArray[index] = currentPlayer.mark;
-
       if(game.checkWinner(currentPlayer.mark)) {
-        console.log(currentPlayer.name + " wins!");
-        game.reset();
+        game.endGameMessage.textContent = currentPlayer.name + " wins!";
+        setTimeout(game.reset, 2500);
       }
       else if(game.checkDraw()) {
-        console.log("It's a draw!");
-        game.reset();
+        game.endGameMessage.textContent = "It's a draw!";
+        setTimeout(game.reset, 2500);
       }
       else {
         game.switchTurn();
       }
     }
   });
-
 
   return { game };
 })();
